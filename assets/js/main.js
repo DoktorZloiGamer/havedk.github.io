ff96407ed127131dc6c331ab7eb050225174a306
havedkconsole <93067435+havedkconsole@users.noreply.github.com>
2024-02-02 09:50:18 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "fC1HF29n9UA"
+const videoId = "96G0ngElPWk"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
